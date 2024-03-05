7a11dbcfdea7d79f02ed2bc5dbb4f486ed16dc00
Ritesh Dixit <ritesh@Riteshs-MacBook-Pro.local>
2024-03-05 21:17:49 +0530

    test files added

---
+++ test/golden_test.dart
@@ -0,0 +1,15 @@
+
+///
+
+/*
+
+Why use Ahem then? Because when you work with multiple OS, the process of font rendering can differ.
+https://nsalleron.medium.com/flutter-pipeline-optimization-with-very-good-test-96952f671557
+
+
+*To avoid the ahem font in production we use golden test
+
+https://pub.dev/packages/golden_toolkit
+
+
+*/
